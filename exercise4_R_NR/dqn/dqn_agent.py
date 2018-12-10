3a73dbb643664ac88a9526942522f463abfc8115
Alex Rose <rosea@tf.uni-freiburg.de>
2018-12-10 16:49:34 +0100

    completed dqn_agent & replay_buffer (for cartpole purposes)

--- exercise4_R_NR/dqn/dqn_agent.py
+++ exercise4_R_NR/dqn/dqn_agent.py
@@ -4,7 +4,7 @@ from dqn.replay_buffer import ReplayBuffer
 
 class DQNAgent:
 
-    def __init__(self, Q, Q_target, num_actions, discount_factor=0.99, batch_size=64, epsilon=0.05):
+    def __init__(self, Q, Q_target, num_actions, discount_factor=0.99, batch_size=64, epsilon=0.05, double_q=False):
         """
          Q-Learning agent for off-policy TD control using Function Approximation.
          Finds the optimal greedy policy while following an epsilon-greedy policy.
@@ -26,6 +26,8 @@ class DQNAgent:
         self.batch_size = batch_size
         self.discount_factor = discount_factor
 
+        self.double_q = double_q
+
         # define replay buffer
         self.replay_buffer = ReplayBuffer()
 
@@ -43,13 +45,41 @@ class DQNAgent:
 
         # TODO:
         # 1. add current transition to replay buffer
-        # 2. sample next batch and perform batch update: 
-        #       2.1 compute td targets: 
-        #              td_target =  reward + discount * argmax_a Q_target(next_state_batch, a)
-        #       2.2 update the Q network
-        #              self.Q.update(...)
-        #       2.3 call soft update for target network
-        #              self.Q_target.update(...)
+        self.replay_buffer.add_transition(state, action, next_state, reward, terminal)
+        
+
+        # 2. sample next batch
+        batch_states, batch_actions, batch_next_states, batch_rewards, batch_dones = self.replay_buffer.next_batch(self.batch_size)
+        
+        # find optimal actions for the sampled s' states
+        if self.double_q:
+            # double Q learning (select actions using current network, rather than target network)
+            # ...in order to decorrelate noise between selection and evaluation
+            # (Q(state,action) is still evaluated using target network in any case)
+            action_selector = self.Q
+        else:
+            action_selector = self.Q_target
+            
+
+        # as usual, the Q network returns a vector of... predicted values for every possible action
+        a_prime = np.argmax(action_selector.predict(self.sess, batch_next_states), axis=1)
+        
+        # pick a''th value from each column of the Q prediction
+        # note, this will include action predictions for "done" state, but we'll kill them later
+        q_values_next = self.Q.predict(self.sess, batch_next_states)[np.arange(self.batch_size), a_prime]
+        
+        # 2.1 compute td targets: 
+        # if done, there will be no next state
+        td_targets = batch_rewards + np.where(batch_dones, 0, discount * q_values_next)
+        
+        
+        # 2.2 update the Q (current) network
+        self.Q.update(self, self.sess, batch_states, batch_actions, td_targets)
+        
+        
+        # 2.3 call soft update for target network
+            # this is done by the dodgy associate_method therein
+        self.Q_target.update(self, self.sess)
    
 
     def act(self, state, deterministic):
@@ -61,17 +91,24 @@ class DQNAgent:
         Returns:
             action id
         """
+        
+        # get action probabilities from current network
+        action_probs = action_selector.predict(self.sess, batch_next_states)
+        
+        
         r = np.random.uniform()
         if deterministic or r > self.epsilon:
-            # TODO: take greedy action (argmax)
-            # action_id = ...
+            # take greedy action
+            action_id = np.argmax(action_probs, axis=1)
         else:
 
-            # TODO: sample random action
+            # sample random action
+
             # Hint for the exploration in CarRacing: sampling the action from a uniform distribution will probably not work. 
             # You can sample the agents actions with different probabilities (need to sum up to 1) so that the agent will prefer to accelerate or going straight.
             # To see how the agent explores, turn the rendering in the training on and look what the agent is doing.
-            # action_id = ...
+
+            action_id = np.random.choice(np.arange(len(action_probs)), p=action_probs)
           
         return action_id
 
--- exercise4_R_NR/dqn/replay_buffer.py
+++ exercise4_R_NR/dqn/replay_buffer.py
@@ -4,19 +4,32 @@ import os
 import gzip
 import pickle
 
+
 class ReplayBuffer:
 
-    # TODO: implement a capacity for the replay buffer (FIFO, capacity: 1e5 - 1e6)
+    # DONE: implement a capacity for the replay buffer (FIFO, capacity: 1e5 - 1e6)
 
     # Replay buffer for experience replay. Stores transitions.
-    def __init__(self):
-        self._data = namedtuple("ReplayBuffer", ["states", "actions", "next_states", "rewards", "dones"])
-        self._data = self._data(states=[], actions=[], next_states=[], rewards=[], dones=[])
+    def __init__(self, capacity=1e5):
+        self._data = namedtuple(
+            "ReplayBuffer",
+            ["states", "actions", "next_states", "rewards", "dones"])
+        # this seems weird - define _data as a tuple, and then redefine it as a new instance of that tuple??
+        # why are we using a named tuple anyway?
+        self._data = self._data(
+            states=[], actions=[], next_states=[], rewards=[], dones=[])
+
+        self.capacity = capacity
 
     def add_transition(self, state, action, next_state, reward, done):
         """
         This method adds a transition to the replay buffer.
         """
+        # check if we're over capacity
+        while len(self._data.states) >= capacity:
+            for d in data:
+                del d[0]  #FIFO
+
         self._data.states.append(state)
         self._data.actions.append(action)
         self._data.next_states.append(next_state)
@@ -29,8 +42,11 @@ class ReplayBuffer:
         """
         batch_indices = np.random.choice(len(self._data.states), batch_size)
         batch_states = np.array([self._data.states[i] for i in batch_indices])
-        batch_actions = np.array([self._data.actions[i] for i in batch_indices])
-        batch_next_states = np.array([self._data.next_states[i] for i in batch_indices])
-        batch_rewards = np.array([self._data.rewards[i] for i in batch_indices])
+        batch_actions = np.array(
+            [self._data.actions[i] for i in batch_indices])
+        batch_next_states = np.array(
+            [self._data.next_states[i] for i in batch_indices])
+        batch_rewards = np.array(
+            [self._data.rewards[i] for i in batch_indices])
         batch_dones = np.array([self._data.dones[i] for i in batch_indices])
         return batch_states, batch_actions, batch_next_states, batch_rewards, batch_dones
